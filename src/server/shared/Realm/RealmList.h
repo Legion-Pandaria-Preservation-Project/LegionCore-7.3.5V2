8087bf23e5923cdd1faddd64be1c1404fa90a666
Terragor <81566364+zTerragor@users.noreply.github.com>
2024-09-20 12:09:44 +0500

    fix address lookup
    fix address lookup for SMSG_CONNECT_TO
    thanks @jasongdove
    https://github.com/jasongdove/LegionCore-7.3.5/commit/e7d55ea68ba898ffd7dad69f0eb2bb553bcca31f

--- src/server/shared/Realm/RealmList.h
+++ src/server/shared/Realm/RealmList.h
@@ -21,8 +21,8 @@
 
 #include "Define.h"
 #include "Realm.h"
-#include <map>
 #include <array>
+#include <map>
 #include <vector>
 #include <unordered_set>
 #include <boost/asio.hpp>
@@ -93,12 +93,11 @@ public:
     Realm const* GetRealm(Battlenet::RealmHandle const& id) const;
 
     RealmBuildInfo const* GetBuildInfo(uint32 build) const;
-    uint32 GetMinorMajorBugfixVersionForBuild(uint32 build) const;
+	uint32 GetMinorMajorBugfixVersionForBuild(uint32 build) const;
     std::unordered_set<std::string> const& GetSubRegions() const { return _subRegions; }
     void WriteSubRegions(bgs::protocol::game_utilities::v1::GetAllValuesForAttributeResponse* response) const;
     std::vector<uint8> GetRealmEntryJSON(Battlenet::RealmHandle const& id, uint32 build) const;
     std::vector<uint8> GetRealmList(uint32 build, std::string const& subRegion, uint8 Battlegroup) const;
-    boost::asio::ip::address GetAddressForClient(uint32 RealmID);
     uint32 JoinRealm(uint32 realmAddress, uint32 build, boost::asio::ip::address const& clientAddress, std::array<uint8, 32> const& clientSecret, LocaleConstant locale, std::string const& os, std::string accountName, bgs::protocol::game_utilities::v1::ClientResponse* response) const;
 
 private:
